fb4b2e7f3b6bc0bac401fb5910bd13d239af6d4f
hathbanger <hathbanger.ah@gmail.com>
2016-02-09 16:24:58 -0700

    added sdut

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -5,16 +5,16 @@ class ApplicationController < ActionController::Base
   # disable this when api time 
   protect_from_forgery with: :exception
 
- protected
-  def authenticate_user!
-    if user_signed_in?
-      super
-    else
-      redirect_to '/users/sign_in'
-      ## if you want render 404 page
-      ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
-    end
-  end
+ # protected
+ #  def authenticate_user!
+ #    if user_signed_in?
+ #      super
+ #    else
+ #      redirect_to '/users/sign_in'
+ #      ## if you want render 404 page
+ #      ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
+ #    end
+ #  end
 # enabled this ::: 
   # protect_from_forgery with: :null_session
   # include DeviseTokenAuth::Concerns::SetUserByToken
