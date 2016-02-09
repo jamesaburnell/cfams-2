40bec73a157d811a09a4ff64e80d45a1ad93d7ec
hathbanger <hathbanger.ah@gmail.com>
2016-02-08 18:25:05 -0700

    stuff

--- server/app/models/post.rb
+++ server/app/models/post.rb
@@ -4,13 +4,12 @@ class Post < ActiveRecord::Base
 	has_attached_file :usr_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 	validates_attachment_content_type :usr_image, content_type: /\Aimage\/.*\Z/	
 
-	# after_initialize :add_source
+	after_initialize :add_source
 
-	# def add_source
-	# 	if self.usr_image != nil
-	# 		self.og_source = self.usr_image.url(:medium)
-	# 		self.image_src = self.usr_image.url(:medium)
-	# 	end
-	# end
+	def add_source
+		if self.og_source == 'change me'
+			self.og_source = self.usr_image.url(:medium)
+		end
+	end
 
 end
